e8c74b2339790c27169f3cf053176bbbf6cbe984
VBernhauer <37836906+VBernhauer@users.noreply.github.com>
2019-06-08 13:39:51 +0200

    Add files via upload

--- problem_19.py
+++ problem_19.py
@@ -1,56 +1,48 @@
 #! /usr/bin/env python
 
-#
-# to run this script in the terminal:
-# ./path/to/script/problem_19.py <from year> <to year>
-#
-# Example:
-# ./path/to/script/problem_19.py 1901 1903 
-# gives the number of Sundays that fell on 1st of the month between 1 Jan 1901 and 31 Dec 1903.
-# 
 
 import numpy as np
-import sys
 
 
-n = int(sys.argv[1])
-m = int(sys.argv[2])
+def get_sundays(n, m):
 
-Jan = 31
-Mar = 31
-Apr = 30
-May = 31
-Jun = 30
-Jul = 31
-Aug = 31
-Sep = 30
-Oct = 31
-Nov = 30
-Dec = 31
+	Jan = 31
+	Mar = 31
+	Apr = 30
+	May = 31
+	Jun = 30
+	Jul = 31
+	Aug = 31
+	Sep = 30
+	Oct = 31
+	Nov = 30
+	Dec = 31
 
-day_count = 0
-total_sundays = 0
-for year in range(1900, m + 1, 1):
+	day_count = 0
+	total_sundays = 0
+	for year in range(1900, m + 1, 1):
 
-	if year % 400 == 0:		
-		Feb = 29
-	elif year % 4 == 0 and year % 100 == 0:
-		Feb = 28
-	elif year % 4 == 0:
-		Feb = 29	
-	else:
-		Feb = 28
+		if year % 400 == 0:		
+			Feb = 29
+		elif year % 4 == 0 and year % 100 == 0:
+			Feb = 28
+		elif year % 4 == 0:
+			Feb = 29	
+		else:
+			Feb = 28
 
-	months = [Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec]
-	# month_no = 0
-	for month in months:
-		# month_no = month_no + 1
-		for day in range(month):
-			day = day + 1	# shift counter by one to start days on 1st
-			day_count = day_count + 1
-			if day_count % 7 == 0 and day == 1 and year >= n:
-					total_sundays = total_sundays + 1
-					# print year, month_no
+		months = [Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec]
+		# month_no = 0
+		for month in months:
+			# month_no = month_no + 1
+			for day in range(month):
+				day = day + 1	# shift counter by one to start days on 1st
+				day_count = day_count + 1
+				if day_count % 7 == 0 and day == 1 and year >= n:
+						total_sundays = total_sundays + 1
+						# print year, month_no
 
 
-print("The number of Sundays that fell on the first of the month during 1 Jan " + str(n) + " to 31 Dec " + str(m) + " was " + str(total_sundays) + ".")
+	print("The number of Sundays that fell on the first of the month during 1 Jan " + str(n) + " to 31 Dec " + str(m) + " was " + str(total_sundays) + ".")
+
+get_sundays(1901, 2000)	
--- problem_6.py
+++ problem_6.py
@@ -1,32 +1,22 @@
 #! /usr/bin/env python
 
-#
-# to run this script in the terminal:
-# ./path/to/script/problem_6.py <integer>
-#
-# Example:
-# ./path/to/script/problem_6.py 100 
-# gives the result for the first one hundred natural numbers.
-# 
+def get_sums(n):
 
-import sys
+	k = 0
+	sum_k = 0
+	sum_sq_k = 0
 
+	for _ in range(n):
+		
+		k = k + 1
+		sum_k = sum_k + k
+		
+		sq_k = k ** 2
+		sum_sq_k = sum_sq_k + sq_k
 
-n = int(sys.argv[1])
+	sum_k_sq = sum_k ** 2
 
-k = 0
-sum_k = 0
-sum_sq_k = 0
+	print("sum of the squares of the first " + str(n) + " natural numbers is " + str(sum_sq_k))
+	print("square of the sum of the first " + str(n) + " natural numbers is " + str(sum_k_sq))
 
-for _ in range(n):
-	
-	k = k + 1
-	sum_k = sum_k + k
-	
-	sq_k = k ** 2
-	sum_sq_k = sum_sq_k + sq_k
-
-sum_k_sq = sum_k ** 2
-
-print("sum of the squares of the first " + str(n) + " natural numbers is " + str(sum_sq_k))
-print("square of the sum of the first " + str(n) + " natural numbers is " + str(sum_k_sq))
+get_sums(100)
